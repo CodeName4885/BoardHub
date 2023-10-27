7fd9a747dda5a00aabd791c7210656009d639033
sariys33 <sariys33@gmail.com>
2023-10-27 15:25:09 +0900

    asd

--- Back/src/main/java/com/ragtag/boardhub/dto/game/GameSortDTO.java
+++ Back/src/main/java/com/ragtag/boardhub/dto/game/GameSortDTO.java
@@ -1,8 +1,12 @@
-package com.ragtag.boardhub.dto.game;
+package com.ragtag.boardhub.DTO.game;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GameSortDTO {
 
     private int tab;
